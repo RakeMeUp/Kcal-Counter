722e86ee142bf4c7a9492af8be2e9fcdd963de86
RakeMeUp <bencehetes@gmail.com>
2022-01-02 17:45:11 +0100

    edit button on elements, clean up, blocking user select

--- src/comps/listElement.jsx
+++ src/comps/listElement.jsx
@@ -1,30 +1,27 @@
 import React, { Component } from 'react'
 import help from '../img/Help.svg'
 import Meter from '../comps/meter'
+import edit from '../img/edit.png'
 
 export default class listElement extends Component {
     constructor(props)
     {
         super(props);
-        this.state = { flag: false };
+        this.state = { 
+            listElementOpen: false,
+        };
 
     }
     
-    handleClick = () => {
-        if(this.state.flag){
-            /* OPEN IT */
-            this.setState({
-                flag: false
-            });
-        }else{
-            /* CLOSE IT */
-            this.setState({
-                flag: true
-            })
-        }
+    handleListElementClick = () => {
+        this.setState({ listElementOpen: !this.state.listElementOpen });
     }
 
-    additional(){
+    cornerRounder = () =>{
+        return this.state.listElementOpen ? "rounded-br-[80px]" : ""
+    }
+
+    additionalComponent(){
         return (
             <div className=' mt-4 relative before:absolute before:bg-paletteLightGray before:h-[1px] before:w-full before:rounded-full'>
                 <div className='pt-4 flex justify-evenly text-sm font-medium text-paletteGray'>                
@@ -47,28 +44,41 @@ export default class listElement extends Component {
 
     render() {
         return (
-            <div onClick={this.handleClick} className={`rounded-[30px] my-2 bg-white shadow-md px-5 py-4`}>
-                <div className='flex w-full justify-between'>
-                    {/* ITEM NAME */}
-                    <div className='flex flex-col'>
-                        <span className='text-2xl'>Banana</span>
-                        <span className='text-lg text-paletteGray'>400g</span>
-                    </div>
-                    {/* ITEM KPP SCORE */}
-                    <div className='flex flex-col'>
-                        <div className='flex'>
-                            <span className='text-2xl '>KPP</span>
-                            <img className='ml-1' src={help} alt="help" width="14px" />
+            <div className='relative'>
+                {/* function in class so i can toggle the corner rounding when open */}
+                <div onClick={this.handleListElementClick} className={`relative z-10 rounded-[30px] ${this.cornerRounder()} my-2 bg-white shadow-md px-5 py-4`}>
+                    <div className='flex w-full justify-between'>
+
+                        {/* ITEM NAME */}
+                        <div className='flex flex-col'>
+                            <span className='text-2xl select-text'>Banana</span>
+                            <span className='text-lg text-paletteGray'>400g</span>
                         </div>
-                        <span className='text-lg text-paletteGray'>5.6</span>
+
+                        {/* ITEM KPP SCORE */}
+                        <div className='flex flex-col'>
+                            <div className='flex'>
+                                <span className='text-2xl '>KPP</span>
+                                <img className='ml-1' src={help} alt="help" width="14px" />
+                            </div>
+                            <span className='text-lg text-paletteGray'>5.6</span>
+                        </div>
+
+                        {/* ITEM SCORE METER */}
+                        <div className='relative'>
+                            <Meter meterNumber={this.props.meter} />
+                        </div>
                     </div>
-                    {/* ITEM SCORE METER */}
-                    <div className='relative'>
-                        <Meter meterNumber={this.props.meter} />
+
+                    {/* AdditionalComponent Information (displays on click) */}
+                    {this.state.listElementOpen && this.additionalComponent()} 
+                </div>
+                {/* Displays when element Open */}
+                {this.state.listElementOpen && 
+                    <div className='z-1 bg-paletteRed absolute bottom-0 right-0 w-20 h-20 flex items-end justify-end rounded-md'>
+                        <img className='w-5 m-1' src={edit} alt="asd" />
                     </div>
-                 </div>
-                {/* Additional Information (displays on click) */}
-                {this.state.flag && this.additional()} 
+                }
             </div>
         )
     }
