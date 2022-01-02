01b3c19ee2f68c0d17e2ffbe9242e9375ab35715
RakeMeUp <bencehetes@gmail.com>
2022-01-02 17:46:34 +0100

    quick z-index fix

--- src/comps/navbar.jsx
+++ src/comps/navbar.jsx
@@ -7,7 +7,7 @@ export default class navbar extends Component {
     render() {
         return (
             /* NAVBAR */
-            <div className='z-10 flex justify-between w-full h-[100px] bg-white fixed bottom-0 rounded-t-[30px] drop-shadow-[0px_-2px_10px_rgba(0,0,0,0.25)]'>
+            <div className='z-50 flex justify-between w-full h-[100px] bg-white fixed bottom-0 rounded-t-[30px] drop-shadow-[0px_-2px_10px_rgba(0,0,0,0.25)]'>
                 {/* Center Button */}
                 <div className='left-0 right-0 mx-auto flex justify-center items-center w-[100px] h-[100px] bg-white border-2 border-paletteGray rounded-full absolute bottom-[25px]'>
                     <img src={plus} alt="PLUS" width="34px" />
