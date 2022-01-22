434b477b9afe39fdab97d6a31d3bb2a8863ae260
RakeMeUp <bencehetes@gmail.com>
2022-01-22 12:30:53 +0100

    this shit again

--- src/comps/currencyButton.jsx
+++ src/comps/currencyButton.jsx
@@ -11,34 +11,11 @@ export default class currencyButton extends Component {
         }
     }
 
-    ClosedList=()=>{
-        return (
-            <button onClick={()=>{this.setState({listOpen: !this.state.listOpen})}} className=' h-10 w-32 flex items-center justify-between bg-white rounded-full figShadow'>
-                <span className='ml-5 text-sm font-light text-paletteGray'>{this.props.currentCurrency ? this.props.currentCurrency : "HUF"}</span>
-                <img className='mr-3' src={downArrow} alt="more" />
-            </button>
-        )
-    }
-
     ListItem=(props)=>{
         return (
             <li onClick={()=>{this.props.handleCurrencyChange(props.currency); this.setState({listOpen: !this.state.listOpen})}} className='h-10 border-b-[1px] border-paletteLightGray mx-6 flex items-center justify-center'>
                 {props.currency}
             </li>
-        )
-    }
-
-    OpenedList=()=>{
-        return (
-            <ol className='h-48 w-32 bg-white rounded-[20px] figShadow'>
-                <this.ListItem currency={"USD"}/>
-                <this.ListItem currency={"HUF"}/>
-                <this.ListItem currency={"EUR"}/>
-                <this.ListItem currency={"GBP"}/>
-                <li onClick={()=>{this.setState({listOpen: !this.state.listOpen})}} className='h-7 flex items-center justify-center'>
-                    <img src={upArrow} alt="back" />
-                </li>
-            </ol>
         )
     }
 
@@ -56,7 +33,21 @@ export default class currencyButton extends Component {
                         modal-body w-64 h-72'>
                             <span className='text-2xl font-medium'>Currency</span>
                             <div className='w-full h-full flex justify-center mt-5 bg-paletteYellow'>
-                                {this.state.listOpen ? <this.OpenedList/> : <this.ClosedList/>}
+                                {this.state.listOpen ? (
+                                    <ol className='h-48 w-32 bg-white rounded-[20px] figShadow'>
+                                        <this.ListItem currency={"USD"}/>
+                                        <this.ListItem currency={"HUF"}/>
+                                        <this.ListItem currency={"EUR"}/>
+                                        <this.ListItem currency={"GBP"}/>
+                                        <li onClick={()=>{this.setState({listOpen: !this.state.listOpen})}} className='h-7 flex items-center justify-center'>
+                                            <img src={upArrow} alt="back" />
+                                        </li>
+                                    </ol>) : (
+                                        <button onClick={()=>{this.setState({listOpen: !this.state.listOpen})}} className=' h-10 w-32 flex items-center justify-between bg-white rounded-full figShadow'>
+                                            <span className='ml-5 text-sm font-light text-paletteGray'>{this.props.currentCurrency ? this.props.currentCurrency : "HUF"}</span>
+                                            <img className='mr-3' src={downArrow} alt="more" />
+                                        </button>
+                                    )}
                             </div>
                         </div>
                     </div>
